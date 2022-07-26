0404a4c3cff4412a883b2846c7c0fbfbf291d2d6
shankar87dayal <shankar.87.dayal@gmail.com>
2022-07-26 12:08:18 +0530

    delete

--- src/main/java/com/ecom/repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecom.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer>{
-
-}
