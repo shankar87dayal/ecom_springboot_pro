3ef073f29b69c55b6e277090d5f6acb80d28d2fd
shankar87dayal <shankar.87.dayal@gmail.com>
2022-07-30 10:54:34 +0530

    add ordersorted methods

--- src/main/java/com/ecom/config/AppConstants.java
+++ src/main/java/com/ecom/config/AppConstants.java
@@ -8,4 +8,5 @@ public class AppConstants {
 	public static final String  PAGE_SIZE_STRING ="25";
 	public static final String  SORT_BY_STRING = "productId";
 	public static final String  SORT_DIR_STRING = "asc";
+	public static final String  SORT_ORDER_BY_STRING = "orderId";
 }
