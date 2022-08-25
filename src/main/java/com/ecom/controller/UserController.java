e2339e858cded11356702630aa7a0b532dc06260
shankar87dayal <shankar.87.dayal@gmail.com>
2022-08-25 22:31:07 +0530

    Add crossOrigin Annotation on line no - 29

--- src/main/java/com/ecom/controller/UserController.java
+++ src/main/java/com/ecom/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.ecom.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("*")
 public class UserController {
 
 	@Autowired
