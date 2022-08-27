6beeeedab9f9d3fc271a9fc0254dbf026aa7de45
shankar87dayal <shankar.87.dayal@gmail.com>
2022-08-27 18:57:11 +0530

    add one annotation line no-27

--- src/main/java/com/ecom/controller/AuthController.java
+++ src/main/java/com/ecom/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.ecom.security.JwtHelper;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 
 	@Autowired
